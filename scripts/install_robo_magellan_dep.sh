b990dc90d7f3fe83e94d03cb40a52af077cc3cb7
Shu-Nong Wu <shunongwu@gmail.com>
2021-01-20 22:50:39 -0800

    added setup script and mavlink  and mavros

---
+++ scripts/install_robo_magellan_dep.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+## Bash script for setting up ROS Melodic (with Gazebo 9) development environment for PX4 on Ubuntu LTS (18.04).
+## It installs the common dependencies for all targets (including Qt Creator)
+##
+## Installs:
+## - Common dependencies libraries and tools as defined in `ubuntu_sim_common_deps.sh`
+## - MAVROS
+##
+## Adapted this script from PX4 repo. Removed a lot of the stuff related to installing ROS. You still need to
+## do that part on your own
+
+if [[ $(lsb_release -sc) == *"xenial"* ]]; then
+  echo "OS version detected as $(lsb_release -sc) (16.04)."
+  echo "ROS Melodic requires at least Ubuntu 18.04."
+  echo "Exiting ...."
+  return 1;
+fi
+
+# ROS Melodic
+## Gazebo simulator dependencies
+sudo apt-get install protobuf-compiler libeigen3-dev libopencv-dev -y
+
+## Install rosinstall and other dependencies
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+
+# MAVROS: https://dev.px4.io/en/ros/mavros_installation.html
+## Install dependencies
+sudo apt-get install python-catkin-tools python-rosinstall-generator -y
+
+#Install geographiclib
+sudo apt install geographiclib-tools -y
+echo "Downloading dependent script 'install_geographiclib_datasets.sh'"
+# Source the install_geographiclib_datasets.sh script directly from github
+install_geo=$(wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh -O -)
+wget_return_code=$?
+# If there was an error downloading the dependent script, we must warn the user and exit at this point.
+if [[ $wget_return_code -ne 0 ]]; then echo "Error downloading 'install_geographiclib_datasets.sh'. Sorry but I cannot proceed further :("; exit 1; fi
+# Otherwise source the downloaded script.
+sudo bash -c "$install_geo"
