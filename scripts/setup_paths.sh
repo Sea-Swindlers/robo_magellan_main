fb48d59a8b7ac350df2dd4026e9e1e4da7be4731
Shu-Nong Wu <shunongwu@gmail.com>
2021-01-31 22:40:09 -0800

    Renamed a bunch of models to include rm in front because its really difficult to uri include submodels into a gazebo model using absolute paths for some reason. Added a very raw bumper plugin that puslished raw collision data.

--- scripts/setup_paths.sh
+++ scripts/setup_paths.sh
@@ -1,6 +1,6 @@
 cd ..
 export RM_CATKIN_WS_SRC_PATH="$(pwd)"
-export RM_MODELS_PATH="$(pwd)/models"
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$(pwd)/models
+export RM_MODELS_PATH="$(pwd)/rm_models"
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$(pwd)/rm_models
 cd scripts
 
